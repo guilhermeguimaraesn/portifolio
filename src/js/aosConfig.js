192392bc4a65b7621311775e6e04c28d2eddd249
Guilherme Guimarães <90528608+guilhermeguimaraesn@users.noreply.github.com>
2024-06-19 09:41:40 -0300

    inicial commit

---
+++ src/js/aosConfig.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init({
+        duration: 1000, // Duração da animação em milissegundos
+        once: false,    // Permite animação ao entrar e sair da visualização
+    });
+});
